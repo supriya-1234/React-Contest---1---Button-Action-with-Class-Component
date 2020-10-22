1fab8d9956601447742311211d70fbea754de6d2
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-10-22 21:00:49 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,25 @@ import "../styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state={
+      para:false
+    };
   }
-
+  renderPara(){
+    return (
+      <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+    );
+  }
   render() {
-    const paragraph =
-      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+    
+      
     return (
       <div id="main">
         {/* Do not remove this main div!! */}
-        <button id="click">
-          <p id="para">{paragraph}</p>
+        <button id="click" onClick={()=>{this.setState({para:true});}}>Click
+      
         </button>
+        {this.state.para?this.renderPara:""}
       </div>
     );
   }
